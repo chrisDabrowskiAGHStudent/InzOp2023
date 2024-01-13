8b5c843e76812cd282da951f22c4ff206bb74f71
chrisDabrowskiAGHStudent <147701542+chrisDabrowskiAGHStudent@users.noreply.github.com>
2024-01-13 18:42:08 +0100

    Update funkcje.cpp

--- funkcje.cpp
+++ funkcje.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <exception>
 #include "funkcje.h"
+
 using namespace std;
 
 double ln(double num) {
@@ -37,9 +38,36 @@ long int ex(int num, int expo) {
 }
 
 double fmed(double* list, int size) {
+	double* sortedList = sort(list, size);
+	double med;
+	if (size % 2 == 0) {
+		int sizel = size/2, sizer = (size/2) + 1;
+		double lhs = sortedList[sizel], rhs = sortedList[sizer];
+		med = (lhs + rhs) / 2;
+	}
+	else med = sortedList[size / 2];
 	//FIND MEDIAN
+	return med;
 }
 
 double* sort(double* list, int size) {
+	int i, j;
+	bool swapped;
+	for (i = 0; i < size - 1; i++) {
+		swapped = false;
+		for (j = 0; j < size - i - 1; j++) {
+			if (list[j] > list[j + 1]) {
+				double temp = list[j];
+				list[j] = list[j + 1];
+				list[j + 1] = temp;
+				swapped = true;
+			}
+		}
 
+		// If no two elements were swapped
+		// by inner loop, then break
+		if (swapped == false)
+			break;
+	}
+	return list;
 }
