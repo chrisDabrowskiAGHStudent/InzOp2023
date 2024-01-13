0820fb043cb214bafdf921ada251f37e10eddeeb
chrisDabrowskiAGHStudent <147701542+chrisDabrowskiAGHStudent@users.noreply.github.com>
2024-01-13 18:41:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,7 +17,16 @@ int main() {
 		h[i] = rand() % 32;
 		cout << h[i] << " ";
 	}
-	cout << endl << "Odchylenie standardowe listy: " << sd(h, 12) << endl;
+	cout << endl << endl << "Sortowana lista: ";
+	h = sort(h, 12);
+
+	for (int i = 0; i < 12; i++)
+	{
+		cout << h[i] << " ";
+	}
+
+	cout << endl << "Odchylenie standardowe listy: " << sd(h, 12);
+	cout << endl << "Mediana listy: " << fmed(h, 12) << endl << endl;
 
 
 	cout << "Eksponent 2^8 = " << ex(2, 8) << endl;
