67c59a11876f73b4ded2a3a124378588191c003a
chrisDabrowskiAGHStudent <147701542+chrisDabrowskiAGHStudent@users.noreply.github.com>
2024-01-13 18:41:10 +0100

    Update UnitTest1.cpp

--- UnitTest1.cpp
+++ UnitTest1.cpp
@@ -1,6 +1,6 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-#include "../Kalkulator/funkcje.cpp"
+#include "../calc/funkcje.cpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -19,7 +19,32 @@ namespace UnitTest1
 			Assert::AreEqual(expected, testResult);
 		}
 		TEST_METHOD(TestMethod2) {
+			double expected = 1;
+			double testResult = ex(9, 1);
 
-		};
+			Assert::AreEqual(expected, testResult);
+		}
+		TEST_METHOD(TestMethod3) {
+			double list [9] =  { 1, 4, 5, 2, 7, 7, 8, 3, 2 };
+			int size = 9;
+			double expected = 2.4037008503093;
+			double testResult = sd(list, 9);
+			Assert::AreEqual(expected, testResult);
+		}
+		TEST_METHOD(TestMethod4) {
+			double list[9] = { 1, 4, 5, 2, 7, 7, 8, 3, 2 };
+			int size = 9;
+			double expected = 4;
+			double testResult = fmed(list, 9);
+			Assert::AreEqual(expected, testResult);
+		}
+		TEST_METHOD(TestMethod5) {
+			double list[9] = { 1, 4, 5, 2, 7, 7, 8, 3, 2 };
+			int size = 9;
+			double expVal[9] = { 1, 2, 2, 3, 4, 5, 7, 7, 8 };
+			double* expected = expVal;
+			double* testResult = sort(list, 9);
+			Assert::AreEqual(expected, testResult);
+		}
 	};
 }
